20d55c8dc87f5b1f1d9ebe0bad669fce70ef0729
Dinh Van Nam <30413547+NamDinhRobotics@users.noreply.github.com>
2020-12-15 15:20:52 +0900

    Update main.cpp

--- testGpu2/main.cpp
+++ testGpu2/main.cpp
@@ -1,32 +1,8 @@
-//
+// This code is used to calcualte the result of A/B matrix using GPU c++
 // File: main.cu
 //
 // GPU Coder version                    : 2.0
 // CUDA/C/C++ source code generated on  : 15-Dec-2020 11:16:10
-//
-
-//***********************************************************************
-// This automatically generated example CUDA main file shows how to call
-// entry-point functions that MATLAB Coder generated. You must customize
-// this file for your application. Do not modify this file directly.
-// Instead, make a copy of this file, modify it, and integrate it into
-// your development environment.
-//
-// This file initializes entry-point function arguments to a default
-// size and value before calling the entry-point functions. It does
-// not store or use any values returned from the entry-point functions.
-// If necessary, it does pre-allocate memory for returned values.
-// You can use this file as a starting point for a main function that
-// you can deploy in your application.
-//
-// After you copy the file, and before you deploy it, you must make the
-// following changes:
-// * For variable-size function arguments, change the example sizes to
-// the sizes that your application requires.
-// * Change the example values of function arguments to the values that
-// your application requires.
-// * If the entry-point functions return values, store these values or
-// otherwise use them as required by your application.
 //
 //***********************************************************************
 
